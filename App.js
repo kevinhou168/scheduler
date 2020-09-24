0e0050ceefbab486acdb99e7db12177d1cfe0a26
kevinhou168 <kevinhou168@gmail.com>
2020-09-24 12:50:19 -0500

    Title display

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>Welcome to my app!</Text>
+      <Text style={styles.bannerStyle}>{schedule.title}</Text>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bannerStyle: {
+    color: '#888',
+    fontSize: 32,
+  }
 });
 
-
+export default App;
